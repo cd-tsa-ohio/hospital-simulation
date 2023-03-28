96da801cd42b9ee631d9aa9b3088832cff2dfda5
sormaz <sormaz@ohio.edu>
2023-03-27 20:56:30 -0400

    only indentation changes

--- dp-space-search/src/edu/ohiou/dynamic/DPExample1.java
+++ dp-space-search/src/edu/ohiou/dynamic/DPExample1.java
@@ -37,14 +37,14 @@ import edu.ohiou.mfgresearch.labimp.spacesearch.TravelingSalesman;
  */
 public class DPExample1 extends ComparableSpaceState {	
 	double totaleffectivness;
-   ArrayList <Integer> decisions= new ArrayList<Integer>();
-	
+	ArrayList <Integer> decisions= new ArrayList<Integer>();
+
 
 	static double values[][] = { { 0, 45, 70, 90, 105,120 }, { 0, 20, 45, 75,110,150 },
 			{ 0, 50, 70, 80, 100,130 } };
-	
-	
- 	public DPExample1(DPExample1 s,int decision) {
+
+
+	public DPExample1(DPExample1 s,int decision) {
 		decisions=new ArrayList<Integer>(s.decisions);	
 		decisions.add(decision);
 		parent = s;
@@ -63,50 +63,50 @@ public class DPExample1 extends ComparableSpaceState {
 		decisions=new ArrayList<Integer>();
 	}
 	public String toString() {
-		 
-				return super.toString() + "DP"+ decisions + "->" + totaleffectivness;
-				
-			}
+
+		return super.toString() + "DP"+ decisions + "->" + totaleffectivness;
+
+	}
 	@Override
 	public double evaluate() {
 		for (int i=0;i<decisions.size();i++) {
-			 totaleffectivness+= values[i][decisions.get(i)];								
+			totaleffectivness+= values[i][decisions.get(i)];								
 		}		 System.out.print("\n" + toString() + ">Total effectivness is" + totaleffectivness);
 		return totaleffectivness;
 	}
 	@Override
-//	public boolean canBeGoal() {
-//		return decisions.size() == 3;
-//	}
+	//	public boolean canBeGoal() {
+	//		return decisions.size() == 3;
+	//	}
 	//decide which one is better
 	public boolean isBetterThan(Searchable inState) {
 		if (inState== null)
 			return false;
 		return this.totaleffectivness>= ((DPExample1) inState).totaleffectivness;
-		
+
 	}
-	
+
 	public boolean canBeGoal() {
 		return decisions.size() ==3;
 	}
-	
+
 	@Override
 	public boolean equals(Searchable s) {
 		DPExample1 sse2 = (DPExample1) s;
 		// TODO Auto-generated method stub
 		return  decisions.equals(sse2.decisions);
 	}
-	
+
 	public Comparator getComparator() {
 
-			return new DPComparator();
+		return new DPComparator();
 
-	  }
-	
+	}
+
 	public int hashCode() {
 		return  decisions.hashCode();	
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DPExample1 ss = new DPExample1();
@@ -123,30 +123,30 @@ public class DPExample1 extends ComparableSpaceState {
 		}
 		bs.setApplet();
 		bs.display("Showing Mandvi space search, searcher is " + a);
-		
+
 
 	}
-	
+
 	@Override
 	public Set<Searchable> makeNewStates() {
 		Set<Searchable>  states=  new HashSet<Searchable>();
-		
+
 		DPExample1 s1= new DPExample1(this,0);
 		DPExample1 s2= new DPExample1(this,1);	
 		DPExample1 s3= new DPExample1(this,2);	
 		DPExample1 s4= new DPExample1(this,3);	
 		DPExample1 s5= new DPExample1(this,4);	
 		DPExample1 s6= new DPExample1(this,5);	
-		
+
 		if (s1.isFeasible()) states.add(s1);
-		
-		
+
+
 		if (s2.isFeasible()) states.add(s2);
 		if (s3.isFeasible()) states.add(s3);
 		if (s4.isFeasible()) states.add(s4);
 		if (s5.isFeasible()) states.add(s5);
 		if (s6.isFeasible()) states.add(s6);
-		
+
 		return states;
 	}
 
@@ -154,9 +154,9 @@ public class DPExample1 extends ComparableSpaceState {
 		int sum=0;
 		for (int i: decisions) {
 			sum+=i;
-			
+
 		}
-	
+
 		return sum<=5 && decisions.size()<=3;
 	}
 
@@ -167,18 +167,18 @@ public class DPExample1 extends ComparableSpaceState {
 		return searchTypes;
 	}
 
-public void init () {
-	panel = new Example2Panel (this);
-}
+	public void init () {
+		panel = new Example2Panel (this);
+	}
 }
 
 class Example2Panel extends JPanel {
-	
+
 	public Example2Panel  (DPExample1 dpe) {
-		
+
 		Object [] names = {"a", "b", "c", "d", "e", "f"};
 		Object [][] vals = new Object [dpe.values.length][dpe.values[0].length];
-		
+
 		for (int i = 0; i< vals.length; i++) {
 			for (int j = 0; j < vals[0].length; j++ ) {
 				vals[i][j] = dpe.values[i][j];
@@ -187,9 +187,9 @@ class Example2Panel extends JPanel {
 		JTable t = new JTable(new DefaultTableModel(vals, names));
 		this.add(t);
 	}
-	
-	
-	
+
+
+
 }
 
 class DPComparator implements Comparator {
