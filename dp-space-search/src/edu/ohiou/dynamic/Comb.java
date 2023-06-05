4d98609e984c99df7d462d74917f48b0146efe0b
sormaz <sormaz@ohio.edu>
2023-06-05 09:30:17 -0400

    removed println, adsed broken flag, still not working

--- dp-space-search/src/edu/ohiou/dynamic/Comb.java
+++ dp-space-search/src/edu/ohiou/dynamic/Comb.java
@@ -13,102 +13,97 @@ public class Comb
 		// TODO Auto-generated constructor stub
 	}
 
-	
-public static 	List<List<Integer>> createCombinations(List<Integer> iterable, int r) throws Exception 
-{
-	List<List<Integer>>resList = new ArrayList<>();
-//	Console c = System.console();
-	int count =0;
-	
-	List<Integer> runList =new ArrayList<>();
-	int n=iterable.size();
-	if (r>n)
+
+	public static 	List<List<Integer>> createCombinations(List<Integer> iterable, int r) throws Exception 
 	{
-		return resList;
-		
-	}
-	
-	int [] indices=new int[r]; // line 40 in python
-	for (int jj= 0; jj<r;jj++)
-	{
-		indices[jj]=jj;
-	}
-	
-	 for (int i = 0; i < r; i++)
-	 {
-		runList.add(iterable.get(indices[i])) ;
-     }
-	 resList.add(runList);// line 41
-	 
-    //System.out.print(resList);
-	 
-	boolean a = true;
-	
-	while (a)
-	{
-		runList =new ArrayList<>();
-		int i =0;
-		for ( i = r - 1; i >= 0; i--)
+		List<List<Integer>>resList = new ArrayList<>();
+		int count =0;
+
+		List<Integer> runList =new ArrayList<>();
+		int n=iterable.size();
+		if (r>n)
 		{
-			if ( indices[i] != i + n - r)
+			return resList;
+
+		}
+
+		int [] indices=new int[r]; // line 40 in python
+		for (int jj= 0; jj<r;jj++)
+		{
+			indices[jj]=jj;
+		}
+
+		for (int i = 0; i < r; i++)
+		{
+			runList.add(iterable.get(indices[i])) ;
+		}
+		resList.add(runList);// line 41
+
+		//System.out.print(resList);
+
+		boolean a = true;
+
+		while (a)
+		{
+			runList =new ArrayList<>();
+			int i =0;
+			for ( i = r - 1; i >= 0; i--)
 			{
-				break;
+				boolean broken = false;
+				if ( indices[i] != i + n - r)
+				{
+					broken = true;
+					break;
+				}
+
+				if (!broken) {
+					return resList;
+				}
+			}		
+			indices[i] += 1;	
+
+			for (int j = i+1 ; j < r; j++) {
+				indices[j] = indices[j - 1] + 1;
 			}
-		
-			
+			System.out.println("indices: " + Arrays.toString(indices));
+
+			for (int ii = 0; ii < r; ii++)
+			{
+				runList.add(iterable.get(indices[ii])) ;
+			}
+			resList.add(runList);
+			System.out.println("run list " + runList);
+			count++;
+			System.out.println("count " + count);
+			if (count > 15) {
+				throw new Exception("aborting infintie loop");
+			}
+		}
+
+
+
 		return resList;
-		
-		}		
-		indices[i] += 1;	
-		
-		for (int j = i+1 ; j < r; j++) {
-			indices[j] = indices[j - 1] + 1;
+
+	}
+
+	public static void main(String[] args)
+	{
+
+		List<Integer>a = new ArrayList<>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		a.add(4);
+		a.add(5);
+		Comb b= new Comb();
+		try {
+			System.out.print( b.createCombinations(a, 3));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		System.out.println("indices: " + Arrays.toString(indices));
-//		System.out.print("enter something>");
-//		
-//
-//		String  ab = c.readLine();
 
-		for (int ii = 0; ii < r; ii++)
-		 {
-			runList.add(iterable.get(indices[ii])) ;
-	     }
-		resList.add(runList);
-		System.out.println("run list " + runList);
-		count++;
-		System.out.println("count" + count);
-		if (count > 15) {
-			throw new Exception("aborting infintie loop");
-		}
-		}
-		
-	
-
-	return resList;
-
-}
-
-public static void main(String[] args)
-{
-//	Console c = System.console();
-//	String  ab = c.readLine();
-	
-	List<Integer>a = new ArrayList<>();
-  a.add(1);
-  a.add(2);
-  a.add(3);
-  a.add(4);
-  a.add(5);
-  Comb b= new Comb();
- try {
-	System.out.print( b.createCombinations(a, 3));
-} catch (Exception e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-
-}
+	}
 }
 
 
