cf06da3ccb892842bb6da85787ab9ccfce473168
Mandvi <mm286614@ohio.edu>
2024-09-20 11:15:18 -0400

    Removed unnecessary constraint from Model 2

--- Gurobi/ILPModel2V2.py
+++ Gurobi/ILPModel2V2.py
@@ -22,30 +22,33 @@ for region in range(len(x)):  # Iterate over regions (outer list)
 totalPatients = gp.quicksum(var for var in y.values())+gp.quicksum(var for var in z.values())
 model.setObjective(totalPatients, GRB.MAXIMIZE)
 #Constraints
+#Constraint 1 : Patient coming from own region and patient taken from other region should be less than the capacity
 for r in range(len(x)):  # Iterate over regions
-      # Iterate over patients
+      # Iterate over patients,
         for i in range(1, len(capacity[r]) + 1): #Iterate over days
             model.addConstr(gp.quicksum(y[r, m] * x[r][m][i]for m in range(len(x[r])))
              +gp.quicksum(gp.quicksum(z[r, r2, m2] * x[r2][m2][i] for m2 in range(len(x[r2])) ) for r2 in range(len(x))if r2!=r)
                 <= capacity[r][i])
 
-#ensuring patient transferred go to one region
-for r in range(len(x)):
-    for r2 in range(len(x)):
-        for i in range(1, len(capacity[r]) + 1):
-            if r2!=r:
-                model.addConstr(gp.quicksum(z[r,r2,m2]  for m2 in range(len(x[r2])))<=1)
-                model.addConstr(gp.quicksum(z[r, r2, m2] for m2 in range(len(x[r2]))) <= gp.quicksum(
-                x[r2][m2][i] for m2 in range(len(x[r2]))))
+#constraint 2:
+#ensuring patient transferred go to one region so we iterating over from region and from patients , and then in the end summinng over to regions
+for r2 in range(len(x)):
+    for m2 in range(len(x[r2])):
+        for i in range(1, len(capacity[r2]) + 1):#gettting days
+                model.addConstr(gp.quicksum(z[r,r2,m2]  for r in range(len(x))if r2!=r) <=1)
+                #model.addConstr(gp.quicksum(z[r, r2, m2] for m2 in range(len(x[r2]))) <= gp.quicksum(
+               # x[r2][m2][i] for m2 in range(len(x[r2]))))
 
 # for r in range(len(x)):
 #     for m in range(len(x[r])):
 #          model.addConstr(z[r,r,m]==0)
-for r in range(len(x)):
-        for m in range(len(x[r])):
+#Constraint patient transffered from 1 region to other region or patient accepted should not be more than 1 , ensuring patient can either be transferred or accetpted
+
+for r2 in range(len(x)):
+        for m in range(len(x[r2])):
             #doing quicksum here because there are days the patient willl come, if i dont do it it will make the days the patient comealso zero
-            model.addConstr(y[r, m] <= gp.quicksum(x[r][m][i] for i in range(1,len(capacity[r])+1)))
-            model.addConstr(gp.quicksum(z[r2, r, m] for r2 in range(len(x)) if r2!=r)+y[r,m]<=1)
+            #model.addConstr(y[r, m] <= gp.quicksum(x[r][m][i] for i in range(1,len(capacity[r])+1)))
+            model.addConstr(gp.quicksum(z[r, r2, m] for r in range(len(x)) if r!=r2)<=1-y[r2,m])
 
 #either you can transfer or accept the same patient you cant transfer the accepted patient
 model.update()
