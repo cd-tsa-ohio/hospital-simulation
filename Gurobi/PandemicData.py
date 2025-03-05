9811cbee0bca29a6a900534526c585e0377cda2c
Mandvi <mm286614@ohio.edu>
2025-03-05 06:17:50 -0500

    changes made to data creation file and weibull parameters

--- Gurobi/PandemicData.py
+++ Gurobi/PandemicData.py
@@ -1,16 +1,20 @@
-#from plotly.graph_objs import Scattergeo, Layout
+
+
+from sqlite3 import Time
 import pandas as pd
-import numpy as np
-import openpyxl
-
-file_path=("C:/Users/HP/Ohio University/Sormaz, Dusan - malik-shared/PhD - Research/DataFiles/COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_Facility_--_RAW_20240226.csv")
-df=pd.read_csv(file_path)
+from tkinter import filedialog
+from decouple import config
+import ou_file_utils as ofu
+import time
+df=pd.read_excel(ofu.getFile())
 pd.set_option('display.max_columns',100)
-new_df=df[(df['state']=='OH')& (df['zip']==45701)]
-newFile=("C:/Users/HP/Ohio University/Sormaz, Dusan - malik-shared/PhD - Research/DataFiles/Athens.csv")
+county=input("County name: ")
+state=input ("state: ")
+#zip=input ("zip number")
+new_df=df[(df['Province_State']==state)& (df['Admin2']==county)]
+transposeddf=new_df.T
+newFile=f"{county}{time.time()}.xlsx"
 try:
-    new_df.to_csv(newFile)
+    transposeddf.iloc[12:].to_excel(newFile,sheet_name=newFile)
 except Exception as e:
-    print("file could not be saved")
-
-print (df['state'].head(3), df ['zip'].head(4), sep="#")
+    print("file count  could not be saved")
--- Gurobi/WeibullParameters.py
+++ Gurobi/WeibullParameters.py
@@ -14,86 +14,7 @@ def weibullparameters (estimatedparams, mean, std):
     equation2= lam**2 * (gamma(1 + 2/k) - (gamma(1 + 1/k))**2) - std**2
     return equation1,equation2
 
-# def predictlos(ageGroup,resource1,resource2):
-#    #we need to  change this
-#     if ageGroup==0:
-#         if resource1==1 & resource2==0:
-#             initialguess = (1.5, 4)
-#             mean_icu = 3.6
-#             std_icu = 4.1
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1==1&resource2==1:
-#             initialguess = (1.5, 18)
-#             mean_icu = 17.5
-#             std_icu = 11.8
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1 == 0 & resource2 == 1:
-#             initialguess = (1.5, 13)
-#             mean_icu = 13.2
-#             std_icu = 14.0
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
 
-#     if ageGroup==1:
-#         if resource1 == 1 & resource2 == 0:
-#             initialguess = (1.5, 6)
-#             mean_icu = 5.9
-#             std_icu = 5.9
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1 == 1 & resource2 == 1:
-#             initialguess = (1.5, 20)
-#             mean_icu = 19.5
-#             std_icu = 14.3
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1 == 0 & resource2 == 1:
-#             initialguess = (1.5, 15)
-#             mean_icu = 15.4
-#             std_icu = 14.2
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#     if ageGroup == 2:
-#         if resource1 == 1 & resource2 == 0:
-#             initialguess = (1.5, 8)
-#             mean_icu = 8.3
-#             std_icu = 7.7
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1 == 1 & resource2 == 1:
-#             initialguess = (1.5, 17)
-#             mean_icu = 17.1
-#             std_icu = 13.5
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1 == 0 & resource2 == 1:
-#             initialguess = (1.5, 14)
-#             mean_icu = 13.6
-#             std_icu = 13.8
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#     if ageGroup == 3:
-#         if resource1 == 1 & resource2 == 0:
-#             initialguess = (1.5, 10)
-#             mean_icu = 10
-#             std_icu = 9.4
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1 == 1 & resource2 == 1:
-#             initialguess = (1.5, 13)
-#             mean_icu = 12.6
-#             std_icu = 10.8
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#         if resource1 == 0 & resource2 == 1:
-#             initialguess = (1.5, 11)
-#             mean_icu = 11.4
-#             std_icu = 11.4
-#             k_icu, k_lam = fsolve(weibullparameters, initialguess, args=(mean_icu, std_icu))
-#             value = np.random.weibull(k_icu) * k_lam
-#     return np.round(value).astype(int)
 stats = {0 : { 10 : {'initialguess' : (1.5,4),'mean': 3.6, 'stdev' : 4.1},
                1 : {'initialguess' : (1.5,13),'mean': 13.2, 'stdev' : 14.0},
                11 : {'initialguess' : (1.5,18),'mean': 17.5, 'stdev' : 11.8}
@@ -107,11 +28,9 @@ stats = {0 : { 10 : {'initialguess' : (1.5,4),'mean': 3.6, 'stdev' : 4.1},
                11 : {'initialguess' : (1.5,17),'mean': 17.1, 'stdev' : 13.5}},
          3: {10 : {'initialguess' : (1.5,10),'mean': 10, 'stdev' : 9.4},
                1 : {'initialguess' : (1.5,11),'mean': 11.4, 'stdev' : 11.4},
-               11 : {'initialguess' : (1.5,13),'mean': 12.6, 'stdev' : 10.8}},
+               11 : {'initialguess' : (1.5,13),'mean': 12.6, 'stdev' : 10.8}}
          
-        4: {10 : {'initialguess' : (1.5,9),'mean': 10, 'stdev' : 9.5},
-               1 : {'initialguess' : (1.5,13),'mean': 13.4, 'stdev' : 13.8},
-               11 : {'initialguess' : (1.5,17),'mean': 17.3, 'stdev' : 13.1}}
+        
         }
 
 
@@ -124,29 +43,29 @@ def predParams():
         initialguess = values['initialguess']
         mean = values['mean']
         stdev = values['stdev']
-        k, lam = fsolve(weibullparameters, initialguess, args=(mean, stdev))
-
-        
-        param[outkey].append({inkey:[k,lam]})
+        k, lam = fsolve(weibullparameters, initialguess, args=(mean, stdev))        
+       # param[outkey].append({inkey:[k,lam]})
+        param[outkey].append([k,lam])
  return param      
  
 
-def predLoss(ageGroup,resnum,resources):
-    params=predParams()   
-    k = params[ageGroup][resnum][resources][0]
-    lam = params[ageGroup][resnum][resources][1]
-    los_data=[]
+def predLoss(ageGroup,resources,params):
+       
+    k = params[ageGroup][resources][0]
+    
+    lam = params[ageGroup][resources][1]
+    #los_data=[]
     value =( np.random.weibull(k) * lam)
-    for i in range (1000):
-        value =( np.random.weibull(k) * lam)
-        los_data.append(value)
-    df=pd.DataFrame(los_data)
-    file_name = "results.xlsx"
-    df.to_excel(file_name, index=False)
+    #for i in range (1000):
+     #   value =( np.random.weibull(k) * lam)
+      #  los_data.append(value)
+    #df=pd.DataFrame(los_data)
+    #file_name = "results.xlsx"
+    #df.to_excel(file_name, index=False)
     return int(value)
 if __name__== "__main__":
     
-    predLoss(0,0,10)
+    predLoss(0,0)
    
     
 
